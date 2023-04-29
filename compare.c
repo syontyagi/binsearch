24d4a7dcebd2a5c5c3e116f02ef5f5b2a3acea61
syontyagi <syontyagi1109@gmail.com>
2023-04-29 12:25:56 -0700

    updated compare.c

--- compare.c
+++ compare.c
@@ -4,10 +4,11 @@
 
 int compare(int a[], int i, int searchterm)
 {
-	if(a[i] == searchterm) {
-		return i;
+	if (a[i] == searchterm) {
+		return 0;
 	}
-	else {
+	else if (a[i] < searchterm) {
 		return -1;
 	}
+	return 1;
 }
